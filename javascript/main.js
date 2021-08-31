af2e82b6e6c2686b1ae61575d1c266b82eecf7e1
PersonajeTm <personajetm@gmail.com>
2021-08-31 17:33:01 -0500

    revertion

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,10 @@
 // game in games folder
 $("document").ready(function() {
   // VARIABLES
-  let sWidth = $(screen).innerWidth();
-  let sHeight = $(screen).innerHeight();
+  let sWidth = $(window).width();
+  let sHeight = $(window).height();
+  $("#root").css("width", sWidth);
+  $("#root").css("height", sHeight);
   // IF STATEMENTS
   if (sHeight >= sWidth){
     showMobileDiv();
@@ -24,8 +26,8 @@ $("document").ready(function() {
   $(window).resize(function() {
     //resize just happened, pixels changed
     // UPDATE WINDOW SIZES
-    sWidth = $(screen).innerWidth();
-    sHeight = $(screen).innerHeight();
+    sWidth = $(window).width();
+    sHeight = $(window).height();
     // IF MOBILE
     if (sHeight >= sWidth){
       showMobileDiv();
@@ -34,5 +36,7 @@ $("document").ready(function() {
     if (sHeight < sWidth) {
       hideMobileDiv();
     }
+    $("#root").css("width", sWidth);
+    $("#root").css("height", sHeight);
   });
 });
